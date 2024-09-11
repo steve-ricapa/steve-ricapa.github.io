5b0f3ee35db89672e193417b3f37eb3c2861c6b4
steve-ricapa <147581880+steve-ricapa@users.noreply.github.com>
2024-09-10 19:00:32 -0500

    Update anim.js

--- anim.js
+++ anim.js
@@ -1,8 +1,7 @@
-// Sincronizar las letras con la canción
+//By Steve_Ricapa
 var audio = document.querySelector("audio");
 var lyrics = document.querySelector("#lyrics");
 
-// Array de objetos que contiene cada línea y su tiempo de aparición en segundos
 var lyricsData = [
     { "text": "I'm findin' ways to articulate the feeling I'm goin' through", "time": 26 },
     { "text": "I just can't say I don't love you", "time": 31 },
@@ -78,7 +77,7 @@ var lyricsData = [
 
 ];
 
-// Animar las letras
+// Animacion de letras
 function updateLyrics() {
   var time = Math.floor(audio.currentTime);
   var currentLine = lyricsData.find(
@@ -86,15 +85,12 @@ function updateLyrics() {
   );
 
   if (currentLine) {
-    // Calcula la opacidad basada en el tiempo en la línea actual
-    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
+    var fadeInDuration = 0.1; 
     var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
 
-    // Aplica el efecto de aparición
     lyrics.style.opacity = opacity;
     lyrics.innerHTML = currentLine.text;
   } else {
-    // Restablece la opacidad y el contenido si no hay una línea actual
     lyrics.style.opacity = 0;
     lyrics.innerHTML = "";
   }
@@ -102,16 +98,14 @@ function updateLyrics() {
 
 setInterval(updateLyrics, 1000);
 
-//funcion titulo
-// Función para ocultar el título después de 216 segundos
 function ocultarTitulo() {
   var titulo = document.querySelector(".titulo");
   titulo.style.animation =
-    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+    "fadeOut 3s ease-in-out forwards"; 
   setTimeout(function () {
     titulo.style.display = "none";
-  }, 3000); // Espera 3 segundos antes de ocultar completamente
+  }, 3000); 
 }
 
-// Llama a la función después de 216 segundos (216,000 milisegundos)
+// Llamado de funcion(test)(216,000 milisegundos)
 setTimeout(ocultarTitulo, 216000);
